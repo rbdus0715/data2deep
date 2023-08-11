a1712f2d1878eddfb9c0ca9393688ad0d21f4211
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-08-12 08:47:11 +0900

    Create evaluation.py

---
+++ team-note/evaluation.py
@@ -0,0 +1,12 @@
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+
+def get_clf_eval(y_test, pred=None, pred_proba=None):
+    confusion = confusion_matrix(y_test, pred)
+    accuracy = accuracy_score(y_test, pred)
+    precision = precision_score(y_test, pred)
+    recall = recall_score(y_test, pred)
+    f1 = f1_score(y_test, pred)
+    roc_auc = roc_auc_score(y_test, pred_proba)
+    
+    print('오차 행렬\n', confusion)
+    print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f}, F1: {3:.4f}, AUC: {4:.4f}\n'.format(accuracy, precision, recall, f1, roc_auc))
