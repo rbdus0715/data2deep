9aee4f8473cc4b9055241f83be8b9a80a8a8aba7
Gyuyeon Cho <85426187+rbdus0715@users.noreply.github.com>
2023-08-12 22:47:55 +0900

    Update evaluation.py

--- team-note/evaluation.py
+++ team-note/evaluation.py
@@ -10,3 +10,34 @@ def get_clf_eval(y_test, pred=None, pred_proba=None):
     
     print('오차 행렬\n', confusion)
     print('정확도: {0:.4f}, 정밀도: {1:.4f}, 재현율: {2:.4f}, F1: {3:.4f}, AUC: {4:.4f}\n'.format(accuracy, precision, recall, f1, roc_auc))
+
+
+from sklearn.metrics import precision_recall_curve
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+def precision_recall_curve_plot(y_test, pred_proba_positive):
+
+    '''prcision_recall_curve_plot
+    정밀도와 재현율 트레이드오프 관계를 나타내는 그래프 그리기
+    - 이진분류에서 주로 사용
+    
+    파라미터 : 정답 배열, Positive 칼럼의 예측 확률 배열
+    - pred_proba_positive = model.predict_proba(X_test)[:, 1]
+    '''
+    
+    precisions, recalls, thresholds = precision_recall_curve(y_test, pred_proba_positive)
+
+    plt.figure(figsize=(8,6))
+    threshold_boundary = thresholds.shape[0]
+    plt.plot(thresholds, precisions[0:threshold_boundary], linestyle='-', label='precision')
+    plt.plot(thresholds, recalls[0:threshold_boundary], label='recall')
+
+    start, end = plt.xlim()
+    plt.xticks(np.round(np.arange(start, end, 0.1), 2))
+
+    plt.xlabel('Threshold value')
+    plt.ylabel('Precision and Recall value')
+    plt.legend()
+    plt.grid()
+    plt.show()
